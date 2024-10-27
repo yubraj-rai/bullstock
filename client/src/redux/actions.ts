38a54895e99ba3f90a5eb1c0ff2c92cd93bc612b
Dhru29 <Dhru29patel@gmail.com>
2024-10-27 10:01:12 -0400

    -- Transctions feature

--- client/src/redux/actions.ts
+++ client/src/redux/actions.ts
@@ -2,6 +2,7 @@ export const AUTH = 'AUTH';
 export const LOGOUT = 'LOGOUT';
 export const UPDATE_USERNAME = 'UPDATE_USERNAME';
 export const OWNED_STOCKS = 'OWNED_STOCKS';
-
+export const UPDATE_STOCK = 'UPDATE_STOCK';
+export const UPDATE_BALANCE = 'UPDATE_BALANCE';
 
 
